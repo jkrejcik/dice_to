107d4abd51dcc31a59a38a94ea0226c085229e50
Jan Krejcik <jan-krejcik@outlook.com>
2023-03-05 19:53:53 +0100

    Update seed file to add users and movies.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require "open-uri"
+
+start = Time.now
+
+puts "=== DESTROYING TABLES ==="
+
+User.destroy_all
+puts "Users deleted"
+
+Movie.destroy_all
+puts "Movies deleted"
+
+puts "=== SEEDING TABLES ==="
+
+puts "=== CREATING USERS ==="
+user1 = User.create(first_name: "Jan", last_name: "Krejcik", email: "jan-krejcik@outlook.com", password: "password",
+  password_confirmation: "password")
+puts "- #{user1.first_name}"
+
+user2 = User.create(first_name: "Ronan", last_name: "Boyle", email: "ronanoboyle.rob@gmail.com", password: "password",
+  password_confirmation: "password")
+puts "- #{user2.first_name}"
+
+user3 = User.create(first_name: "Jaro", last_name: "Sidor", email: "jaroslav.sidor.ml@gmail.com", password: "password",
+  password_confirmation: "password")
+puts "- #{user3.first_name}"
+
+puts "=== CREATING MOVIES ==="
