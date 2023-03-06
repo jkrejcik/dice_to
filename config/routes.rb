71453cf20136cec305fcddd94dd8d487e7d07c21
Jaroslav Sidor <jaroslav.sidor.ml@gmail.com>
2023-03-06 21:08:00 +0100

    added results controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'results/index'
+  get 'results/edit'
+  get 'results/update'
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
