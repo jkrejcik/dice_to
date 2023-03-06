847cacfbd8bf5b18c5a4976acb4145b624cad49f
Jaroslav Sidor <jaroslav.sidor.ml@gmail.com>
2023-03-06 21:05:23 +0100

    create actions for movie results and customer results controllers

--- app/controllers/custom_results_controller.rb
+++ app/controllers/custom_results_controller.rb
@@ -1,2 +1,10 @@
 class CustomResultsController < ApplicationController
+  def new
+  end
+
+  def show
+  end
+
+  def create
+  end
 end
--- app/controllers/movie_results_controller.rb
+++ app/controllers/movie_results_controller.rb
@@ -1,2 +1,13 @@
 class MovieResultsController < ApplicationController
+  def question
+  end
+
+  def create_suggestion
+  end
+
+  def show
+  end
+
+  def create
+  end
 end
