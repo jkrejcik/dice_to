e096dc7672e4132d2c9419aac2d0938ba612f19b
Jan Krejcik <jan-krejcik@outlook.com>
2023-03-07 17:16:58 +0100

    added as well category page and action + missing create view file for movie_results

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  # Routes for pages
   root to: "pages#home"
+  get "categories", to: "pages#category"
 
   # Routes for movie category
   get "movie-questions", to: "movie_results#question"
   post "movie-questions", to: "movie_results#create_suggestion"
   get "suggestion", to: "movie_results#show"
+  get "movie-accept", to: "movie_results#create"
 end
