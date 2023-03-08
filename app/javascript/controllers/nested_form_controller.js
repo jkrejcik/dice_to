045e57dfd4301bde30e02b1c90bee0e31146c0ed
Ronan OBoyle <ronanoboyle.rob@gmail.com>
2023-03-08 23:49:12 +0000

    reformatted and set up for completing javascript

--- app/javascript/controllers/nested_form_controller.js
+++ app/javascript/controllers/nested_form_controller.js
@@ -2,19 +2,24 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="nested-form"
 export default class extends Controller {
-  static targets = ["template", "target"]
+  static targets = ["template", "target", "plus", "minus"]
 
   connect() {
+    console.log(this.templateTarget.plusTarget)
   }
 
   add(event) {
-    console.log(this.templateTarget.value)
-    this.targetTarget.insertAdjacentHTML("beforeend", this.templateTarget.innerHTML)
+    // console.log(this.targetTarget)
+    this.plusTarget.classList.add("d-none")
+    this.minusTarget.classList.remove("d-none")
+    this.targetTarget.insertAdjacentHTML("beforeend", this.templateTarget.innerHTML )
   }
 
   remove(event) {
-    console.log(this.targetTarget)
+    // console.log(this.targetTarget)
     // this.targetTarget.removeAdjacentHTML("beforeend", this.templateTarget.innerHTML)
     this.targetTarget.remove(this.templateTarget.innerHTML)
   }
+
+
 }
