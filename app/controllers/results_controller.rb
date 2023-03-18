a85c10f8ad1c062771902a286c93a4be4c14dc83
Jan Krejcik <jan-krejcik@outlook.com>
2023-03-18 22:39:30 +0100

    first draft of slide card for history page

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,7 +1,7 @@
 class ResultsController < ApplicationController
   def index
-    @movie_results = MovieResult.all.order('created_at DESC')
-    @custom_results = CustomResult.all.order('created_at DESC')
+    @movie_results = MovieResult.all.order('created_at DESC').where(user: current_user)
+    @custom_results = CustomResult.all.order('created_at DESC').where(user: current_user)
     @today = Time.now.utc
   end
 
