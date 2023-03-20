2e271dd3fbdd6a454e2abc87b6758783fd81843e
Ronan OBoyle <ronanoboyle.rob@gmail.com>
2023-03-20 19:04:14 +0000

    initial controller tweaks

--- app/javascript/controllers/question_controller.js
+++ app/javascript/controllers/question_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="question"
 export default class extends Controller {
-  static targets = ["next", "all"]
+  static targets = ["next", "all", "step"]
 
   connect() {
     // something on connection is flashing before the following. 
@@ -16,12 +16,12 @@ export default class extends Controller {
   next(event) {
     let answeredPartial = document.getElementsByClassName("show")
     if ((this.forms.length) == 2) { event.target.style.display = 'none' }
-      answeredPartial[0].classList.add("d-none")
-      answeredPartial[0].classList.remove("tab", "show")
-      let forms = document.getElementsByClassName("tab")
-      let randomTabIndex = Math.floor(Math.random() * forms.length)
-      forms[randomTabIndex].classList.remove("d-none")
-      forms[randomTabIndex].classList.add("show")
+    answeredPartial[0].classList.add("d-none")
+    answeredPartial[0].classList.remove("tab", "show")
+    let forms = document.getElementsByClassName("tab")
+    let randomTabIndex = Math.floor(Math.random() * forms.length)
+    forms[randomTabIndex].classList.remove("d-none")
+    forms[randomTabIndex].classList.add("show")
   }
 
   select(event) {
@@ -31,30 +31,15 @@ export default class extends Controller {
     if (id == "mood") { this.allButtons = document.getElementsByClassName("checkbox-emoji") }
     if (id == "decade") { this.allButtons = document.getElementsByClassName("checkbox-element-decade") }
     Array.from(this.allButtons).forEach((button) => {
-      if (button.id != longID) {
-        button.parentElement.classList.add("d-none")
-      }
+      if (button.id != longID) { button.parentElement.classList.add("d-none") }
     })
     if ((this.forms.length) == 1) {
       let answeredPartial = document.getElementsByClassName("show")
-      console.log(answeredPartial)
       answeredPartial[0].classList.add("d-none")
     }
   }
 
-
-  // select(event) {
-  //   // identify with htmlFor
-  //   console.log(event.target.htmlFor)
-  //   let emojis = this.emojiTargets
-  //   console.log(emojis)
-  //   emojis.forEach((element, index) => {
-  //     if (element.htmlFor = event.target.htmlFor) {
-  //         emojis.splice((index + 1), index)
-  //     }
-  //     // else {
-  //     //   element.attributes[1] = filter: saturate(50%);
-  //     // }
-  //   })
-  // }
+  switch(event) {
+    console.log(event.target)
+  }
 }
