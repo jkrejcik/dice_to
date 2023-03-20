b841d3dba557deb00636be20283d828f53cee59a
Ronan OBoyle <ronanoboyle.rob@gmail.com>
2023-03-20 23:11:40 +0000

    More splitting out of controller

--- app/controllers/movie_results_controller.rb
+++ app/controllers/movie_results_controller.rb
@@ -10,20 +10,12 @@ class MovieResultsController < ApplicationController
   end
 
   def create_suggestion
-    result = Movie.all.sample
-    genre = @colour_genre_key[params[:colour].to_sym] if params[:colour]
-    genre ? genre_candidates = Movie.where(genre: genre) : []
+    @result = Movie.sample
+    @result = set_result if params[:colour] || params[:mood] || params[:decade] || params[:weather]
 
-    year = params[:decade].to_i..(params[:decade].to_i + 10) if params[:decade]
-    year ? year_candidates = Movie.where(year: year) : []
+    redirect_to(movie_questions_path) and return if @result.blank?
 
-    mood = params[:mood] if params[:mood]
-    mood_candidates = set_mood(mood) if mood
-    result = (genre_candidates + mood_candidates + year_candidates).sample if (genre_candidates + mood_candidates + year_candidates).count.positive?
-
-    redirect_to(movie_questions_path) and return if result.blank?
-
-    if MovieResult.create(movie_id: result.id, user: current_user, time_taken: params[:time_taken])
+    if MovieResult.create(movie_id: @result.id, user: current_user, time_taken: params[:time_taken])
       redirect_to suggestion_path
     else
       render "question"
@@ -40,6 +32,34 @@ class MovieResultsController < ApplicationController
 
   def set_movie
     @movie_result = MovieResult.where(user: current_user).last
+  end
+
+  def set_result
+    genre_candidates = set_genre_candidates
+    year_candidates = set_year_candidates
+    mood_candidates = set_mood_candidates
+
+    @result = (genre_candidates + mood_candidates + year_candidates).sample
+  end
+
+  def set_genre_candidates
+    genre = @colour_genre_key[params[:colour].to_sym] if params[:colour]
+    genre ? Movie.where(genre: genre) : []
+  end
+
+  def set_year_candidates
+    year = params[:decade].to_i..(params[:decade].to_i + 10) if params[:decade]
+    year ? Movie.where(year: year) : []
+  end
+
+  def set_mood_candidates
+    mood = params[:mood] if params[:mood]
+    mood ? mood(mood) : []
+  end
+
+  def set_weather_candidates
+    weather = params[:weather] if params[:weather]
+    weather ? set_mood(mood) : []
   end
 
   def set_mood(mood)
@@ -62,6 +82,8 @@ class MovieResultsController < ApplicationController
     @years_for_select = [["60s", 1960], ["70s", 1970], ["80s", 1980], ["90s", 1990], ["2000s", 2000], ["2010s", 2010], ["2020s", 2020]]
     @sad_words = ["%anger%", "%sad%", "%kill%", "%death%", "%fail%", "%cry%"]
     @happy_words = ["%happy%", "%success%", "%joy%", "%dog%", "%family%", "%love%"]
+    @weather_words = [{ cloudy: ["%dark%", "%dreary%", "%grey%", "%overcase%"] }, { clear: ["%light%", "%day%"] },
+                      { sunny: ["%sun%"] }, { rainy: ["%storm%", "%rain%", "%wet%", "%sea%"] }]
     @colours = ["blue", "green", "yellow", "red", "pink", "orange", "brown", "purple"]
     @sqlhappy = @happy_words.map { "overview ilike ?" }.join(" or ")
     @sqlsad = @sad_words.map { "overview ilike ?" }.join(" or ")
