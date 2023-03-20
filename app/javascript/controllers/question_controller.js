20d7594a8b37b405886c6ebd7ab2e9c23a3aea0f
Ronan OBoyle <ronanoboyle.rob@gmail.com>
2023-03-20 14:16:54 +0000

    updated movie questions

--- app/javascript/controllers/question_controller.js
+++ app/javascript/controllers/question_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="question"
 export default class extends Controller {
-  static targets = ["next"]
+  static targets = ["next", "all"]
 
   connect() {
     // something on connection is flashing before the following. 
@@ -15,18 +15,13 @@ export default class extends Controller {
 
   next(event) {
     let answeredPartial = document.getElementsByClassName("show")
-    if ((this.forms.length - 1) == 0) {
-      event.target.style.display = 'none'
-      this.forms[0].classList.add("d-none")
-    }
-    else {
+    if ((this.forms.length) == 2) { event.target.style.display = 'none' }
       answeredPartial[0].classList.add("d-none")
       answeredPartial[0].classList.remove("tab", "show")
       let forms = document.getElementsByClassName("tab")
       let randomTabIndex = Math.floor(Math.random() * forms.length)
       forms[randomTabIndex].classList.remove("d-none")
       forms[randomTabIndex].classList.add("show")
-    }
   }
 
   select(event) {
@@ -40,6 +35,11 @@ export default class extends Controller {
         button.parentElement.classList.add("d-none")
       }
     })
+    if ((this.forms.length) == 1) {
+      let answeredPartial = document.getElementsByClassName("show")
+      console.log(answeredPartial)
+      answeredPartial[0].classList.add("d-none")
+    }
   }
 
 
