81dcd0faacc95dfbc4e8e37a32b62315c01a285f
Jan Krejcik <jan-krejcik@outlook.com>
2023-03-28 00:28:50 +0200

    working restaurant search based on user location and pick of color which represents cuisine

--- app/controllers/restaurant_results_controller.rb
+++ app/controllers/restaurant_results_controller.rb
@@ -1,22 +1,61 @@
 class RestaurantResultsController < ApplicationController
-  before_action :set_restaurant, only: %i[show]
+  # before_action :set_restaurant, only: %i[show]
   before_action :colour, only: %i[question create_suggestion]
-  before_action :set_options, only: %i[question]
+  # before_action :set_options, only: %i[question]
 
   def question
-    @restaurant_result = RestaurantResult.new
+    @restaurant_result = Restaurant.new
   end
 
   def create_suggestion
-    @result = set_result if params[:colour] || params[:mood] || params[:decade] || params[:weather] || params[:star]
-    @result = Restaurant.all.sample if @result.nil?
+    @restaurant_result = Restaurant.new
+    @client = GooglePlaces::Client.new(ENV.fetch('GOOGLE_API'))
+    coordinates = params[:coordinates].split(",")
+    latitude = coordinates[0]
+    longitude = coordinates[1]
 
-    RestaurantResult.create(restaurant_id: @result.id, user: current_user, time_taken: params[:time_taken])
-    redirect_to restaurant_suggestion_path((RestaurantResult.where(user: current_user).last))
+    if params[:colour]
+      cuisine = @colour_cuisine_key[params[:colour].to_sym]
+    else
+      items = @colour_cuisine_key.values
+      cuisine = items[rand(items.length)]
+    end
 
+    restaurant = @client.spots(latitude, longitude, :name => cuisine, :types => 'restaurant', :radius => 500, :detail => true).sample
+
+    @restaurant_result.name = restaurant.name
+    @restaurant_result.city = restaurant.city
+    @restaurant_result.cuisine = cuisine
+    @restaurant_result.address = restaurant.formatted_address
+    @restaurant_result.phone = restaurant.formatted_phone_number
+    @restaurant_result.rating = restaurant.rating
+    @restaurant_result.price = restaurant.price_level
+
+    if restaurant.photos.empty?
+      @restaurant_result.image_1 = "https://source.unsplash.com/random?restaurant"
+      @restaurant_result.image_2 = "https://source.unsplash.com/random?restaurant"
+      @restaurant_result.image_3 = "https://source.unsplash.com/random?restaurant"
+    elsif restaurant.photos.size == 1
+      @restaurant_result.image_1 = restaurant.photos[0].fetch_url(800)
+      @restaurant_result.image_2 = "https://source.unsplash.com/random?restaurant"
+      @restaurant_result.image_3 = "https://source.unsplash.com/random?restaurant"
+    elsif restaurant.photos.size == 2
+      @restaurant_result.image_1 = restaurant.photos[0].fetch_url(800)
+      @restaurant_result.image_2 = restaurant.photos[1].fetch_url(800)
+      @restaurant_result.image_3 = "https://source.unsplash.com/random?restaurant"
+    elsif restaurant.photos.size >= 3
+      @restaurant_result.image_1 = restaurant.photos[0].fetch_url(800)
+      @restaurant_result.image_2 = restaurant.photos[1].fetch_url(800)
+      @restaurant_result.image_3 = restaurant.photos[2].fetch_url(800)
+    end
+    @restaurant_result.save
+
+    RestaurantResult.create(restaurant_id: @restaurant_result.id, user: current_user, time_taken: params[:time_taken])
+    redirect_to restaurant_suggestion_path(RestaurantResult.last)
   end
 
   def show
+    @restaurant_result = RestaurantResult.find(params[:id])
   end
 
   def index
@@ -37,50 +76,50 @@ class RestaurantResultsController < ApplicationController
   private
 
   def set_result
-    cuisine_candidates = set_cuisine_candidates
-    # year_candidates = set_year_candidates
-    # mood_candidates = set_mood_candidates
-    # weather_candidates = set_weather_candidates
-    star_candidates = set_star_candidates
+    # cuisine_candidates = set_cuisine_candidates
+    # # year_candidates = set_year_candidates
+    # # mood_candidates = set_mood_candidates
+    # # weather_candidates = set_weather_candidates
+    # star_candidates = set_star_candidates
 
-    # (cuisine_candidates + mood_candidates + year_candidates + weather_candidates + star_candidates).sample
-    (cuisine_candidates + star_candidates).sample
+    # # (cuisine_candidates + mood_candidates + year_candidates + weather_candidates + star_candidates).sample
+    # (cuisine_candidates + star_candidates).sample
   end
 
   def set_star_candidates
-    star_sign = params[:star] if params[:star]
-    star_sign ? star_sign(star_sign) : []
+    # star_sign = params[:star] if params[:star]
+    # star_sign ? star_sign(star_sign) : []
   end
 
   def star_sign(star_sign)
-    first_letter = star_sign[0]
-    Restaurant.where("title ilike '#{first_letter}%'")
+    # first_letter = star_sign[0]
+    # Restaurant.where("title ilike '#{first_letter}%'")
   end
 
   def set_restaurant
-    @restaurant_result = RestaurantResult.where(user: current_user).last
+    # @restaurant_result = RestaurantResult.where(user: current_user).last
   end
 
   def set_options
-    @restaurant_cuisines = []
-    @years_for_select = [["60s", 1960], ["70s", 1970], ["80s", 1980], ["90s", 1990], ["2000s", 2000], ["2010s", 2010], ["2020s", 2020]]
+    # @restaurant_cuisines = []
+    # @years_for_select = [["60s", 1960], ["70s", 1970], ["80s", 1980], ["90s", 1990], ["2000s", 2000], ["2010s", 2010], ["2020s", 2020]]
   end
 
   def set_cuisine_candidates
-    cuisine = @colour_cuisine_key[params[:colour].to_sym] if params[:colour]
-    cuisine ? Restaurant.where(cuisine: cuisine) : []
+    # cuisine = @colour_cuisine_key[params[:colour].to_sym] if params[:colour]
+    # cuisine ? Restaurant.where(cuisine: cuisine) : []
   end
 
   def colour
     @colour_cuisine_key = {
-      orange: "Chinese", brown: "Mexican", pink: "Jamaican", blue: "Drama", green: "Italian",
-      red: "Taiwanese", yellow: "Spanish", purple: "French", violet: "German", emerald: "Eritrean",
-      grey: "Russian", jasmine: "Japanese", myrtle: "Greek", navy: "Vietnamese", maroon: "Polish", cream: "American"
+      orange: "French", brown: "Chinese", pink: "Chinese", blue: "Italian", green: "Greek",
+      red: "Spanish", yellow: "Lebanese", purple: "Turkey", violet: "Thai", emerald: "Indian",
+      grey: "Mexican", jasmine: "American"
     }
     @colour_hash_key = {
       orange: "#FC7A57", brown: "#5b4b49", pink: "#E56399", blue: "#5C78FF", green: "#01C97B",
-      red: "#FF653E", yellow: "#FFCA14", purple: "#89608E", navy: "#19297C", myrtle: "#297373",
-      cream: "#FFE5D4", grey: "#BDB4BF", violet: "#f0d3f7", emerald: "#26532B", jasmine: "#ffd972", maroon: "#92374d"
+      red: "#FF653E", yellow: "#FFCA14", purple: "#89608E", grey: "#BDB4BF", violet: "#f0d3f7",
+      emerald: "#26532B", jasmine: "#ffd972"
     }
   end
 end
